93683b06f460be77df397749e6ff272eb8efd64a
Felipe Cavalcante Lacerda <felipecavalcantelacerda@hotmail.com>
2023-05-28 00:27:07 -0300

    Added SVG icons

---
+++ src/assets/IconChecked/index.tsx
@@ -0,0 +1,19 @@
+export default function IconChecked() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12.6004 5.00024L6.60042 11.0002L3 7.39982"
+        stroke="#5F41D9"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
