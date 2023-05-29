a9fc316696f652805f754535b337c858580f8537
Felipe Cavalcante Lacerda <felipecavalcantelacerda@hotmail.com>
2023-05-28 23:19:32 -0300

    Ident SVG codes

--- src/assets/IconMenu/index.tsx
+++ src/assets/IconMenu/index.tsx
@@ -1,7 +1,10 @@
 export default function IconMenu() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" color="#ffff">
-      <path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z"></path>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path
+        d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z"
+        fill="rgba(251,247,247,1)"
+      ></path>
     </svg>
   );
 }
