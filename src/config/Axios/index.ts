c70f079468aa8e4bb1d6373c82c1883570834921
Felipe Cavalcante Lacerda <felipecavalcantelacerda@hotmail.com>
2023-05-30 10:13:00 -0300

    Migrating to Vite

--- src/config/Axios/index.ts
+++ src/config/Axios/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const Axios = axios.create({
-  baseURL: `${process.env.REACT_APP_DATA_BASE_URL}`,
+  baseURL: `${import.meta.env.VITE_DATA_BASE_URL}`,
   headers: {
     'Content-Type': 'application/json',
   },
