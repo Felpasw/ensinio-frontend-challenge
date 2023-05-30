071360d7d713a36e1f555c2ba488286b70a862d2
Felipe Cavalcante Lacerda <felipecavalcantelacerda@hotmail.com>
2023-05-30 10:09:38 -0300

    Migrating to Vite

---
+++ src/textContent/index.tsx
@@ -0,0 +1,170 @@
+import IconApp from '../assets/IconApp';
+import IconEad from '../assets/IconEad';
+import IconGamification from '../assets/IconGamification';
+import IconSocial from '../assets/IconSocial';
+
+export const TopTiTleText = {
+  pt: 'Plataforma all in one',
+  en: 'all in one platform',
+  es: 'plataforma todo en uno',
+};
+
+export const TitleText = {
+  pt: 'Sua escola online poderosa e lucrativa',
+  en: 'Your online school powerfull and profitable',
+  es: 'Tu escuela online potente y rentable',
+};
+
+export const SolutionsTitle = {
+  pt: 'Principais soluções',
+  en: 'Main solutions',
+  es: 'Soluciones principales',
+};
+
+export const BtnText = {
+  pt: 'Começar agora',
+  en: 'Start now',
+  es: 'Empezar ahora',
+};
+
+export const SubtitleText = {
+  pt: 'Tenha sua própria escola online 100% white label com rede social, gamificação, clube de assinaturas, ecommerce e sistema EAD completo.',
+  en: 'Have your own 100% white label online school with a social network, gamification, subscription club, ecommerce and complete distance learning system',
+  es: 'Ten tu propia escuela online 100% marca blanca con red social, gamificación, club de suscripción, ecommerce y completo sistema de aprendizaje a distancia.',
+};
+
+export const BottomTitleText = {
+  pt: 'Queremos que o aluno se sinta confortável enquanto aprende',
+  en: 'We want the student to feel comfortable while learning',
+  es: 'Queremos que el alumno se sienta cómodo mientras aprende',
+};
+
+export const BottomSubtitleText = {
+  pt: 'Conheça alguns dos nossos recursos ⚡',
+  en: 'Discover some of our resources ⚡',
+  es: 'Descubre algunos de nuestros recursos⚡',
+};
+
+export const FooterText = {
+  pt: 'Veja todos os outros recursos disponíveis para te ajudar',
+  en: 'See all other resources available to help you',
+  es: 'Vea todos los otros recursos disponibles para ayudarlo',
+};
+
+export const NavItemsList = [
+  {
+    content: {
+      pt: 'Preços',
+      en: 'Prices',
+      es: 'Precios',
+    },
+    Delay: 0.7,
+  },
+  {
+    content: {
+      pt: 'Academy',
+      en: 'Academy',
+      es: 'Academy',
+    },
+  },
+  {
+    content: {
+      pt: 'Blog',
+      en: 'Blog',
+      es: 'Blog',
+    },
+    Delay: 1.1,
+  },
+  {
+    content: {
+      pt: 'Contato',
+      en: 'Contact',
+      es: 'Contacto',
+    },
+    Delay: 1.3,
+  },
+];
+
+export const solutionsList = [
+  {
+    icon: <IconEad />,
+    title: {
+      pt: 'Crie uma Escola Online',
+      en: 'Create an Online School',
+      es: 'Crear una escuela en línea',
+    },
+    descripton: {
+      pt: 'Lorem ipsum dolor sit amet',
+      en: 'Lorem ipsum dolor sit amet',
+      es: 'Lorem ipsum dolor sit amet',
+    },
+  },
+  {
+    icon: <IconGamification />,
+    title: {
+      pt: 'Gamificação',
+      en: 'Gamification',
+      es: 'Gamificación',
+    },
+    descripton: {
+      pt: 'Lorem ipsum dolor sit amet',
+      en: 'Lorem ipsum dolor sit amet',
+      es: 'Lorem ipsum dolor sit amet',
+    },
+  },
+  {
+    icon: <IconSocial />,
+    title: {
+      pt: 'Comunidade e rede social',
+      en: 'Community and social network',
+      es: 'Comunidad y red social',
+    },
+    descripton: {
+      pt: 'Lorem ipsum dolor sit amet',
+      en: 'Lorem ipsum dolor sit amet',
+      es: 'Lorem ipsum dolor sit amet',
+    },
+  },
+  {
+    icon: <IconApp />,
+    title: {
+      pt: 'Aplicativo mobile',
+      en: 'Mobile app',
+      es: 'Aplicación movil',
+    },
+    descripton: {
+      pt: 'Lorem ipsum dolor sit amet',
+      en: 'Lorem ipsum dolor sit amet',
+      es: 'Lorem ipsum dolor sit amet',
+    },
+  },
+];
+export const FooterRightTextContent = {
+  pt: 'Ver mais',
+  en: 'See more',
+  es: 'Ver más',
+};
+
+export const LoginText = {
+  pt: 'Entrar',
+  en: 'Login',
+  es: 'Acceso',
+};
+
+export const SolutionsText = {
+  pt: 'Soluções',
+  en: 'Solutions',
+  es: 'Soluciones',
+};
+
+export const SeeVideoText = {
+  pt: 'Ver vídeo',
+  en: 'Watch video',
+  es: 'Ver video',
+};
+
+export const BottomSubtitleText2 = {
+  pt: 'Pensamos em cada detalhe',
+  en: 'We think of every detail',
+  es: 'Pensamos en cada detalle',
+};
